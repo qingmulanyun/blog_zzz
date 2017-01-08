17b6b7b9196da8fccb1b7fa868f79bbe539937d1
qingmulanyun <wenbo199019@gmail.com>
2017-01-08 15:40:33 +1100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.create([
+                        {
+                            email: 'test-user-00@mail.com',
+                            name: 'test-user-00',
+                            phone: '15102160807'
+                        },
+                        {
+                            email: 'test-user-01@mail.com',
+                            name: 'test-user-01',
+                            phone: '15102160809'
+                        }
+                    ])
