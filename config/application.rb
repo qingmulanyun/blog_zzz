ba28f8196cadba7af20688310e8b3f093e41de5d
Wenbo Zhao <wenbo.zhao@R5001221.local>
2017-01-17 15:47:31 +1100

    fix heroku assets conflicts

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,8 @@ module BlogZzz
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_assets = true
-    config.active_record.raise_in_transactional_callbacks = true
-    config.assets.enabled = true
     config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
