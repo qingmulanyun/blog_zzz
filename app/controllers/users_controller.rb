d13e19e5a28e4d1218ac3348ccd20cd4c26bba87
wenbo zhao <wenbo.zhao@fairfaxmedia.com.au>
2017-05-05 16:09:52 +1000

    add public hub page for login user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
     @posts = User.find(current_user).posts.paginate(:page => params[:page]).order(created_at: :desc)
     @user = User.find(current_user)
   end
+
+  def public_hub
+    @posts = Post.publicPosts.includes(:user, :comments).order(created_at: :desc)
+  end
 end
