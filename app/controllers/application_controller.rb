20e1123cab99dcaaa55ac05f1a8f01c9bd5234f2
qingmulanyun <wenbo199019@gmail.com>
2018-02-18 14:56:10 +1100

    refactoring login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_action :verify_authorized
+  after_action :verify_authorized, unless: :devise_controller?
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
