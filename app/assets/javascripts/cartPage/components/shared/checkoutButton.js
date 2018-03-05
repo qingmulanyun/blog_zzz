69933c005b9742b3c5dc48a57161014a6f5e647b
Wenbo Zhao <wenbo199019@gmail.com>
2018-03-05 15:03:15 +1100

    add order dialog (#79)
    * add order dialog
    
    * order page

--- app/assets/javascripts/cartPage/components/shared/checkoutButton.js
+++ app/assets/javascripts/cartPage/components/shared/checkoutButton.js
@@ -4,11 +4,16 @@ import { withStyles } from 'material-ui/styles';
 import {
     Template, TemplatePlaceholder, Plugin, TemplateConnector,
 } from '@devexpress/dx-react-core';
-
+import Dialog, {
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from 'material-ui/Dialog';
 import Button from 'material-ui/Button';
-
+import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
-
+import { Loading } from '../../../utilities/loadingComponent/loading';
 import blue from 'material-ui/colors/blue';
 
 const styles = theme => ({
@@ -18,21 +23,76 @@ const styles = theme => ({
     highLight: {
         fontSize: "18px",
         color: blue[700],
-        fontWeight: "bold"
+        fontWeight: "bold",
+        marginLeft: "0.5em"
+    },
+    title: {
+        borderBottom: `2px solid ${blue[200]}`,
+        textAlign: "center",
+        color: "#6C6C6C",
+        fontSize: "12px"
+    },
+    content: {
+        textAlign: "center",
+        margin: "1em",
+        padding: "0.5em"
+    },shopName: {
+        padding: "0.5em"
+    },shopContainer: {
+        borderTop: `1px dotted ${blue[200]}`,
+        borderBottom: `1px dotted ${blue[200]}`,
+    },shopSummary: {
+        marginTop: "0.5em"
+    },itemContainer: {
+        backgroundColor: blue[50],
+        margin: "1px",
+        color: "#3c3c3c"
     }
 });
 
 class CheckoutButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orderDialogOpen: false
+        };
+    }
+    handleOpenOrderDialog = () => {
+        this.setState({ orderDialogOpen: true });
+    };
+
+
+    handleCloseOrderDialog = () => {
+        this.setState({ orderDialogOpen: false });
+    };
+
+    handleSubmitOrder = (ids) => {
+        // this.props.deleteItems(ids);
+        this.setState({ orderDialogOpen: false });
+    };
+
+    groupBy = (xs, f) => {
+        var obj = xs.reduce((r, v, i, a, k = f(v)) => ((r[k] || (r[k] = [])).push(v), r), {});
+        var result = $.map(obj, function(value, index) {
+            return [value];
+        });
+        return result
+    };
 
     render (){
-        const { classes, selectedIndexes, rows } = this.props;
+        const { orderDialogOpen } = this.state;
+        const { classes, selectedIndexes, rows, loading } = this.props;
 
         var selectedIds =[];
         var totalPrice = 0;
+        var selectedItems = []
         selectedIndexes.map(function(index){
-            selectedIds.push(rows[index].id)
+            selectedIds.push(rows[index].id);
+            selectedItems.push(rows[index]);
             totalPrice += rows[index].total_price
         });
+
+        var ordersList = this.groupBy(selectedItems, (cartItem) => cartItem.shop_id);
 
         return (
             <Plugin name="AddNewItem">
@@ -50,7 +110,7 @@ class CheckoutButton extends React.Component {
 
                                 <div>
                                     <Typography variant="caption">
-                                        合计（不含运费）<span className={classes.highLight}> ¥{totalPrice} </span>
+                                        合计（不含运费）<span className={classes.highLight}> ¥ {totalPrice.toFixed(2)} </span>
                                     </Typography>
                                 </div>
                                 <div>
@@ -59,6 +119,7 @@ class CheckoutButton extends React.Component {
                                         color="primary"
                                         className={classes.button}
                                         disabled={selectedIds.length === 0}
+                                        onClick={this.handleOpenOrderDialog}
                                     >
                                         结算
                                     </Button>
@@ -68,6 +129,87 @@ class CheckoutButton extends React.Component {
                         )}
                     </TemplateConnector>
                 </Template>
+
+                <Dialog
+                    maxWidth="md"
+                    fullWidth
+                    open={orderDialogOpen}
+                    aria-labelledby="order-list"
+                >
+                    <DialogTitle id="order-list">确认订单信息
+                    </DialogTitle>
+                    <DialogContent>
+                        <Grid container spacing={8}>
+                            <Grid item xs={4}>
+                                <div className={classes.title}>店铺宝贝</div>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <div className={classes.title}>单价</div>
+                            </Grid>
+                            <Grid item xs={1}>
+                                <div className={classes.title}>数量</div>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <div className={classes.title}>运费</div>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <div className={classes.title}>小计</div>
+                            </Grid>
+                        </Grid>
+
+                        {
+                            ordersList.map(function(items_per_shop, index){
+                                var shopTotalPrice = 0;
+                                var shopTotalTransport = 0;
+                                return <div>
+                                    <Typography variant="caption" gutterBottom align="left" className={classes.shopName}>
+                                        {items_per_shop[0].shop_name}
+                                    </Typography>
+                                    <Grid container spacing={8} key={index} className={classes.shopContainer}>
+
+                                        {
+                                            items_per_shop.map(function(item, index){
+                                                shopTotalPrice += item.total_price;
+                                                shopTotalTransport += item.transport_cost;
+                                                return <Grid container spacing={8} key={index} className={classes.itemContainer}>
+                                                    <Grid item xs={4}>
+                                                        <div className={classes.content}>{item.name}</div>
+                                                    </Grid>
+                                                    <Grid item xs={2}>
+                                                        <div className={classes.content}>¥{item.price}</div>
+                                                    </Grid>
+                                                    <Grid item xs={1}>
+                                                        <div className={classes.content}>{item.quantity}</div>
+                                                    </Grid>
+                                                    <Grid item xs={2}>
+                                                        <div className={classes.content}>¥{item.transport_cost}</div>
+                                                    </Grid>
+                                                    <Grid item xs={3}>
+                                                        <div className={classes.content}>¥{item.total_price + item.transport_cost}</div>
+                                                    </Grid>
+                                                </Grid>
+                                            })
+                                        }
+                                    </Grid>
+                                    <Typography variant="body1" gutterBottom align="right" className={classes.shopSummary}>
+                                        店铺合计(含运费)  <span className={classes.highLight}>¥ { (shopTotalPrice + shopTotalTransport).toFixed(2) }</span>
+                                    </Typography>
+                                </div>
+                            })
+                        }
+
+                    </DialogContent>
+                    { loading && <Loading />}
+                    <DialogActions>
+                        <Button onClick={this.handleCloseOrderDialog} color="primary">
+                            取消
+                        </Button>
+                        <Button onClick={(e) => this.handleSubmitOrder(selectedIds)} color="primary">
+                            提交订单
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+
             </Plugin>
         );
     }
