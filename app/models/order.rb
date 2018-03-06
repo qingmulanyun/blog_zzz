9bfdd8e6f4756cba289ddc514221154abbbd0c01
qingmulanyun <wenbo199019@gmail.com>
2018-03-06 23:26:45 +1100

    add orders badges

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ActiveRecord::Base
   has_many :order_items, dependent: :destroy
   belongs_to :shop
   belongs_to :buyer, class_name: 'User'
+
+  scope :new_created, -> { where status: 'new' }
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,4 +4,8 @@ class Shop < ActiveRecord::Base
   has_many :items
 
   has_many :admin_orders, class_name: 'Order', dependent: :destroy
+
+  def new_orders
+    admin_orders.new_created
+  end
 end
