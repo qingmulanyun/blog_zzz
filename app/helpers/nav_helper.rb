5fc4a77d4c076bebfd591cb649bed841c6c64ef2
qingmulanyun <wenbo199019@gmail.com>
2018-05-01 16:22:47 +1000

    move count to helper method

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -7,4 +7,18 @@ module NavHelper
     end
     content_tag(:span, span_context, class: "cart-badges")
   end
+
+  def cart_items_count
+    content_tag(:span, current_user.cart_items_count, class: "cart-badges")
+  end
+
+  def orders_count
+    span_context = current_user.shop.new_orders.count > 0 ? current_user.shop.new_orders.count  : ''
+    content_tag(:span, span_context, class: "cart-badges-orders")
+  end
+
+  def wish_products_count
+    span_context = WishProduct.status_at('approved').count > 0 ? WishProduct.status_at('approved').count : ''
+    content_tag(:span, span_context, class: "cart-badges-orders")
+  end
 end
